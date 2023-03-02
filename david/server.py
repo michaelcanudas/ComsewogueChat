c4641b1ac06268288da07e445ac29625d3acf601
Michael Canudas <michael@canudas.dev>
2023-03-02 17:46:17 -0500

    deeze changes r broken ong

---
+++ david/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from inputformat import format
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def main():
+    try:
+        req = request.get_json()["question"]
+        res = format(req)
+        return res
+    except:
+        return "Invalid Request"
