71586028126e42427dbb17ea87014340c6b77232
Michael Canudas <michael@canudas.dev>
2023-03-03 13:55:54 -0500

    omg

--- david/main.py
+++ david/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from inputformat import format
+from importformat import inputformat
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,7 @@ def main():
     try:
         req = request.get_json()["question"]
         span = bool(request.get_json()["spanish"])
-        res = format(req, span)
+        res = inputformat.format(req, span)
         return res
     except Exception as e:
         return str(e)
