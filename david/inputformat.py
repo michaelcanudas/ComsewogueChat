609af337464d0e56078968fe93ab3fa4fbcb4800
MaxwellCosenza <maxwellcosenza4@gmail.com>
2023-03-04 10:43:21 -0500

    translate

--- david/inputformat.py
+++ david/inputformat.py
@@ -1,22 +1,22 @@
 import re
 from answer import respond
 
-#from googletrans import Translator
+from googletrans import Translator
 
-#def translate(text, target_language):
-    #translator = Translator()
-    #if target_language == "english":
-        #translation = translator.translate(text, dest="en")
-        #return translation.text
-    #elif target_language == "spanish":
-        #translation = translator.translate(text, dest="es")
-        #return translation.text
+def translate(text, target_language):
+    translator = Translator()
+    if target_language == "english":
+        translation = translator.translate(text, dest="en")
+        return translation.text
+    elif target_language == "spanish":
+        translation = translator.translate(text, dest="es")
+        return translation.text
 
 pattern = r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s'
 
 def format(input, span):
-    #if span:
-        #input = translate(str(input), "english")
+    if span:
+        input = translate(str(input), "english")
 
     pastQueries = []
     pastContexts = []
@@ -26,10 +26,8 @@ def format(input, span):
     answers = []
     for sentence in sentences:
         output, pastQueries, pastContexts = respond(sentence, pastQueries, pastContexts)
-        #if span:
-        #    output = translate(str(output), "spanish")
+        if span:
+            output = translate(str(output), "spanish")
         answers.append(output)
 
     return answers
-
-#print(format("a que hora es el juego de futbul americano", True))
