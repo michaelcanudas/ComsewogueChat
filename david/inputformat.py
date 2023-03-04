57f4fe12c113a54c57bafe5e109f907e188ed8d4
MaxwellCosenza <maxwellcosenza4@gmail.com>
2023-03-04 10:59:45 -0500

    span fix

--- david/inputformat.py
+++ david/inputformat.py
@@ -2,6 +2,7 @@ import re
 from answer import respond
 
 from googletrans import Translator
+from unidecode import unidecode
 
 def translate(text, target_language):
     translator = Translator()
@@ -17,6 +18,7 @@ pattern = r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s'
 def format(input, span):
     if span:
         input = translate(str(input), "english")
+        input = input.replace("?", "? ")
 
     pastQueries = []
     pastContexts = []
@@ -28,6 +30,7 @@ def format(input, span):
         output, pastQueries, pastContexts = respond(sentence, pastQueries, pastContexts)
         if span:
             output = translate(str(output), "spanish")
+            output = unidecode(output)
         answers.append(output)
 
     return answers
