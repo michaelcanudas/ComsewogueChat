70e7e79a61afba5e9d4938b0ac0c4927bd503417
Michael Canudas <michael@canudas.dev>
2023-03-03 20:31:57 -0500

    Omg

--- david/app.py
+++ david/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
-from inputformat import format
 from importformat import inputformat
+
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/", methods=["POST"])
 def main():
