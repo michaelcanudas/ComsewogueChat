5e6853265d140cddb5197ed5f97c98ff4b520761
MaxwellCosenza <maxwellcosenza4@gmail.com>
2023-03-05 19:31:40 -0500

    random answer

--- david/formatoutput.py
+++ david/formatoutput.py
@@ -1,6 +1,9 @@
+import constants
+import random
+
 def format_output(queries, context, answers):
     if len(queries) == 0 and len(context) == 0:
-        return "I'm sorry, I don't understand the question."
+        return constants.NO_QC_RESPONSES_1[random.randint(0, 4)] + " " + constants.NO_QC_RESPONSES_2[random.randint(0, 4)]
     if len(queries) == 0:
         return "I'm not sure what you want to know about the " + " ".join(context) + ". Maybe try reformatting your question to include a question word?"
 
@@ -27,7 +30,7 @@ def format_output(queries, context, answers):
         answers_str = " and ".join([", ".join(format_answers[:-1]), format_answers[-1]])
     else:
         answers_str = format_answers[0]
-        
+
     response = "The " + queries_str + " of the " + " ".join(context) + " is " + answers_str + "."
     return response.strip()
 
