af3017f564a80fcb38bfcf1efda608d0a07d211e
MaxwellCosenza <maxwellcosenza4@gmail.com>
2023-03-21 15:08:18 -0400

    small

--- david/responder/ranking/modules/similarity.py
+++ david/responder/ranking/modules/similarity.py
@@ -9,12 +9,15 @@ def rank(ranks, queries, context):
         if count > max_count:
             max_count = count
 
+    reached = False
     for entry in ranks:
         count = len(entry["answer"]["keywords"])
         score = count / max_count
 
         if count > len(context)/3.5:
             entry["score"] += score * SIMILARITY_WEIGHT
-        else:
-            raise NoResultsException(queries, context)
+            reached = True
+
+    if not reached:
+        raise NoResultsException(queries, context)
 
